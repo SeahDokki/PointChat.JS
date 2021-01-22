09b1ba36fac94d5c19cc3805188e13b4cbee5d42
pierre.deroubaix935@gmail.com <aswzdxefc751>
2021-01-22 14:38:08 +0100

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET users listing. */
+router.get('/user', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
